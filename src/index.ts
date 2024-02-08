677fd26fd859cd95c2f8de55994a263959713bf7
tarberd <bernardo.mferrari@gmail.com>
2024-02-08 22:07:09 +0100

    first release of the monthly income statement

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,13 @@ type AccountTree = {
 const non_empty_cell_filter = (cell: string): boolean => cell !== "";
 const non_empty_row_filter = (row: string[]): boolean => row[0] !== "";
 
+const pre_order_traversal = (account: Account, f: (account: Account) => any) => {
+  f(account);
+  for (const child of account.children) {
+    pre_order_traversal(child, f);
+  }
+}
+
 const find_account = (account: Account, name: string): Account | null => {
   for (const child of account.children) {
     if (child.name === name) {
@@ -123,6 +130,13 @@ type Ledger = {
   entries: LedgerEntry[],
 }
 
+type AccountEntry = {
+  date: Date,
+  description: string,
+  type: "debit" | "credit",
+  value: number,
+}
+
 function make_ledger(account_tree: AccountTree, ledger_tables: string[][][]): Ledger {
   const ledger = {
     entries: new Array<LedgerEntry>,
@@ -220,13 +234,13 @@ function make_ledger(account_tree: AccountTree, ledger_tables: string[][][]): Le
 function create_monthly_income_statement(
   account_types: string[][],
   account_table: string[][],
-  currencies: string[],
+  currencies_table: string[][],
   ...ledger_tables: string[][][]
-): string[][] {
-  const currency_set = currencies.reduce((previous, current) => {
-    previous.add(current);
-    return previous;
-  }, new Set<string>());
+): any[][] {
+  const currencies = new Array<string>;
+  for (const currency_entry of currencies_table.filter(non_empty_row_filter)) {
+    currencies.push(currency_entry[0]);
+  }
 
   const ret: string[][] = [];
   
@@ -262,7 +276,7 @@ function create_monthly_income_statement(
   // }
   
   const ledger_by_date = {
-    entries: new Map<[number, number], [LedgerEntry]>,
+    entries: new Map<[number, number], LedgerEntry[]>,
   }
   for (const entry of ledger.entries) {
     const key: [number, number] = [entry.date.getMonth(), entry.date.getFullYear()];
@@ -296,21 +310,176 @@ function create_monthly_income_statement(
   //     }
   //   }
   // }
+  
+  const key_hash = (account: Account, currency: string, date: [number, number]): string => `${account.name}-${currency}-${date.toString()}`;
+  const ledger_by_date_and_account = {
+    entries: new Map<string, AccountEntry[]>, 
+  };
+  
+  for (const [key, ledger_entries] of ledger_by_date.entries) {
+    for (const ledger_entry of ledger_entries) {
+      const kind = ledger_entry.data.kind;
+      if (kind === "default" || kind === "liability") {
+        const currency = ledger_entry.data.currency;
+        const credit_acc = ledger_entry.data.credit_account;
+        const credit_entry: AccountEntry = {
+          date: ledger_entry.date,
+          description: ledger_entry.description,
+          type: "credit",
+          value: ledger_entry.data.value,
+        };
 
+        const credit_account_entries = ledger_by_date_and_account.entries.get(key_hash(credit_acc, currency, key));
+        if (credit_account_entries !== undefined) {
+          credit_account_entries.push(credit_entry);
+        } else {
+          ledger_by_date_and_account.entries.set(key_hash(credit_acc, currency, key), [credit_entry]);
+        }
+
+        const debit_acc = ledger_entry.data.debit_account;
+        const debit_entry: AccountEntry = {
+          date: ledger_entry.date,
+          description: ledger_entry.description,
+          type: "debit",
+          value: ledger_entry.data.value,
+        };
+
+        const debit_account_entries = ledger_by_date_and_account.entries.get(key_hash(debit_acc, currency, key));
+        if (debit_account_entries !== undefined) {
+          debit_account_entries.push(debit_entry);
+        } else {
+          ledger_by_date_and_account.entries.set(key_hash(debit_acc, currency, key), [debit_entry]);
+        }
+      } else if (kind === "exchange") {
+        const credit_currency = ledger_entry.data.credit_currency;
+        const credit_acc = ledger_entry.data.credit_account;
+        const credit_entry: AccountEntry = {
+          date: ledger_entry.date,
+          description: ledger_entry.description,
+          type: "credit",
+          value: ledger_entry.data.credit_value,
+        };
+        const credit_account_entries = ledger_by_date_and_account.entries.get(key_hash(credit_acc, credit_currency, key));
+        if (credit_account_entries !== undefined) {
+          credit_account_entries.push(credit_entry);
+        } else {
+          ledger_by_date_and_account.entries.set(key_hash(credit_acc, credit_currency, key), [credit_entry]);
+        }
+
+        const debit_currency = ledger_entry.data.debit_currency;
+        const debit_acc = ledger_entry.data.debit_account;
+        const debit_entry: AccountEntry = {
+          date: ledger_entry.date,
+          description: ledger_entry.description,
+          type: "debit",
+          value: ledger_entry.data.debit_value,
+        };
+        const debit_account_entries = ledger_by_date_and_account.entries.get(key_hash(debit_acc, debit_currency, key));
+        if (debit_account_entries !== undefined) {
+          debit_account_entries.push(debit_entry);
+        } else {
+          ledger_by_date_and_account.entries.set(key_hash(debit_acc, debit_currency, key), [debit_entry]);
+        }
+
+        const exchange_account = ledger_entry.data.exchange_account;
+        const exchange_debit_entry: AccountEntry = {
+          date: ledger_entry.date,
+          description: ledger_entry.description,
+          type: "debit",
+          value: ledger_entry.data.credit_value,
+        };
+        const exchange_debit_account_entries = ledger_by_date_and_account.entries.get(key_hash(exchange_account, credit_currency, key));
+        if (exchange_debit_account_entries !== undefined) {
+          exchange_debit_account_entries.push(exchange_debit_entry);
+        } else {
+          ledger_by_date_and_account.entries.set(key_hash(exchange_account, credit_currency, key), [exchange_debit_entry]);
+        }
+        const exchange_credit_entry: AccountEntry = {
+          date: ledger_entry.date,
+          description: ledger_entry.description,
+          type: "credit",
+          value: ledger_entry.data.debit_value,
+        };
+        const exchange_credit_account_entries = ledger_by_date_and_account.entries.get(key_hash(exchange_account, debit_currency, key));
+        if (exchange_credit_account_entries !== undefined) {
+          exchange_credit_account_entries.push(exchange_credit_entry);
+        } else {
+          ledger_by_date_and_account.entries.set(key_hash(exchange_account, debit_currency, key), [exchange_credit_entry]);
+        }
+      }
+    }
+  }
+  
   const begin_date = ledger.entries[0].date;
   const begin_month = begin_date.getMonth(); 
   const begin_year = begin_date.getFullYear(); 
+  const begin: [number, number] = [begin_month, begin_year];
   const end_date = ledger.entries[ledger.entries.length - 1].date;
   const roll_over = end_date.getMonth() + 1 === 12; 
   const end_month = roll_over ? 0 : end_date.getMonth() + 1;
   const end_year = roll_over ? end_date.getFullYear() + 1 : end_date.getFullYear(); 
+  const end: [number, number] = [end_month, end_year];
   
-  for (let year = begin_year; year < end_year; year++) {
-    for (let month = begin_month; month < end_month; month++) {
+  for (const currency of currencies) {
+    ret.push(["currency", currency]);
+    const months = [...generate_months(begin, end)].reverse();
+    const months_header: any[] = [""];
+    for (const [month, year] of months) {
+      months_header.push(new Date(year, month))
+    }
+    ret.push(months_header);
+    for (const [account, account_type] of account_tree.root_accounts) {
+      if (account_type.statement === "income_statement") {
+        pre_order_traversal(account, (acc) => {
+          const ret_entry: any[] = [acc.name];
+          for (const key of months) {
+            const entries = ledger_by_date_and_account.entries.get(key_hash(acc, currency, key));
+            let total = 0;
+            if(entries !== undefined) {
+              for(const entry of entries) {
+                if (account_type.kind == "normal_credit") {
+                  if (entry.type === "credit") {
+                    total += entry.value;
+                  } else {
+                    total -= entry.value;
+                  }
+                } else {
+                  if (entry.type === "debit") {
+                    total += entry.value;
+                  } else {
+                    total -= entry.value;
+                  }
+                }
+              }
+            }
+            ret_entry.push(total);
+          }
+          ret.push(ret_entry);
+        });
+      }
     }
   }
 
   return ret;
+}
+
+function *generate_months(
+  [begin_month, begin_year]: [number, number],
+  [end_month, end_year]: [number, number]
+  ): IterableIterator<[number, number]> {
+    let year = begin_year;
+    let month = begin_month;
+    while (true) {
+      yield [month, year];
+      month = month + 1;
+      if (month % 12 === 0)  {
+        month = 0;
+        year = year + 1;
+      }
+      if (year === end_year && month === end_month) {
+        break;
+      }
+    }
 }
 
 function collect_account_names(account_list: string[], current_acc: string | null, account: Account) {
